f670cc6dec3d51201ba2d8c79a21e3196cf58b5d
Sondre Gundersrud <sgundersrud@outlook.com>
2026-05-04 15:48:24 +0200

    Update register form: change form ID to name for consistency and clarity

--- account/login.js
+++ account/login.js
@@ -7,3 +7,13 @@ x.type = "text";
 x.type = "password";
 }
 }
+
+/* Set logged-in status */
+navigator.login.setStatus("logged-in");
+
+/* Set logged-out status */
+navigator.login.setStatus("logged-out");
+
+//code is taken from https://mollify.noroff.dev/content/feu1/javascript-1/module-4/managing-web-forms?nav=course (Module 4, Managing web Forms with JavaScript) and modified to fit the project.
+
+const form = document.getElementById("loginForm");
